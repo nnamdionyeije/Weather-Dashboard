6da1affae8ffbbdfb98c6f9fe78e18b52ada1a3a
nnamdionyeije <81874532+nnamdionyeije@users.noreply.github.com>
2023-02-24 13:34:22 -0500

    Added base files

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+
+
+
+// function getApi() {
+//     var weatherUrl = 'api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}'
+// }
+
+
+// geocoder API
+// api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
+
+
+// current weather
+// https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
